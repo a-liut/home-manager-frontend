c18bcf2354e4a9b5ec5cb2495116ae784b39429a
Aliut <andrea.liut@gmail.com>
2019-11-22 11:14:13 +0100

    feat: used UriComponentsBuilder to build service URI. feat: device service url as app property.

--- src/main/java/it/aliut/hmfrontend/service/DeviceService.java
+++ src/main/java/it/aliut/hmfrontend/service/DeviceService.java
@@ -3,31 +3,46 @@ package it.aliut.hmfrontend.service;
 import it.aliut.hmfrontend.entity.Device;
 import it.aliut.hmfrontend.entity.DeviceData;
 import it.aliut.hmfrontend.exception.DeviceNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @Service
 public class DeviceService {
 
+    private final String baseUrl;
+
     private final RestTemplate restTemplate;
 
-    public DeviceService(RestTemplateBuilder restTemplateBuilder) {
+    public DeviceService(RestTemplateBuilder restTemplateBuilder, @Value("${app.services.devices.url}") String baseUrl) {
         restTemplate = restTemplateBuilder.build();
+        this.baseUrl = baseUrl;
     }
 
     public Device[] getAllDevices() {
-        String url = "http://localhost:8080/devices";
-        return restTemplate.getForObject(url, Device[].class);
+        URI uri = getUriBuilder()
+                .path("/{devicesPath}")
+                .buildAndExpand("devices")
+                .toUri();
+
+        return restTemplate.getForObject(uri, Device[].class);
     }
 
 
     public Device getById(String id) {
-        String url = "http://localhost:8080/devices/" + id;
+        URI uri = getUriBuilder()
+                .path("/{devicesPath}/{deviceId}")
+                .buildAndExpand("devices", id)
+                .toUri();
+
         try {
-            return restTemplate.getForObject(url, Device.class);
+            return restTemplate.getForObject(uri, Device.class);
         } catch (HttpClientErrorException ex) {
             if (ex.getStatusCode() == HttpStatus.NOT_FOUND) {
                 throw new DeviceNotFoundException(id);
@@ -40,10 +55,13 @@ public class DeviceService {
     public DeviceData[] getAllDataForDevice(Device device) {
         if (device == null) throw new IllegalArgumentException("Invalid device");
 
-        String url = "http://localhost:8080/devices/" + device.getId() + "/data";
+        URI uri = getUriBuilder()
+                .path("/{devicesPath}/{deviceId}/{dataPath}")
+                .buildAndExpand("devices", device.getId(), "data")
+                .toUri();
 
         try {
-            return restTemplate.getForObject(url, DeviceData[].class);
+            return restTemplate.getForObject(uri, DeviceData[].class);
         } catch (HttpClientErrorException ex) {
             if (ex.getStatusCode() == HttpStatus.NOT_FOUND) {
                 throw new DeviceNotFoundException(device.getId());
@@ -51,5 +69,9 @@ public class DeviceService {
 
             throw ex;
         }
+    }
+
+    private UriComponentsBuilder getUriBuilder() {
+        return UriComponentsBuilder.fromHttpUrl(baseUrl);
     }
 }
